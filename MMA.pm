c8e803229b12cfb846a1c773ad109862722352fb
Craig MacKenna <craigm014@gmail.com>
2010-02-28 06:09:19 +0000

    Import of MACKENNA/IPC-MMA-0.81 from CPAN.
    gitpan-cpan-distribution: IPC-MMA
    gitpan-cpan-version:      0.81
    gitpan-cpan-path:         MACKENNA/IPC-MMA-0.81.tar.gz
    gitpan-cpan-author:       MACKENNA
    gitpan-cpan-maturity:     released

--- MMA.pm
+++ MMA.pm
@@ -8,7 +8,7 @@ require DynaLoader;
 our @ISA = qw(Exporter DynaLoader);
 
 # set the version for version checking
-our $VERSION = 0.8;
+our $VERSION = 0.81;
 
 # nothing is exported into callers namespace by default
 our @EXPORT = qw( );
